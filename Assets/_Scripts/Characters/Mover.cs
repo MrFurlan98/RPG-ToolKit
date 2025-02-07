18b5ce486e42fb32349195807f8fb6f070b15c03
Vinicius Furlan <mr.furlan98@gmail.com>
2025-02-07 16:39:06 -0300

    refactor: Separate the control layer

--- Assets/_Scripts/Characters/Mover.cs
+++ Assets/_Scripts/Characters/Mover.cs
@@ -8,33 +8,17 @@ public class Mover : MonoBehaviour
     [SerializeField] private Animator _animator;
     private NavMeshAgent _agent;
 
-    private Ray _lastRay;
-
-    // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
-    {
-        if (Input.GetMouseButton(0))
-            MoveToCursor();
-
+    {        
         UpdateAnimator();
     }
 
-    private void MoveToCursor()
-    {
-        _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(_lastRay, out hit))
-            MoveToPosition(hit.point);
-    }
-
-    private void MoveToPosition(Vector3 position)
+    public void MoveTo(Vector3 position)
     {
         if (_agent)
             _agent.SetDestination(position);
---
+++ Assets/_Scripts/Control/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Mover _playerMover;
+
+    private Ray _lastRay;
+
+    private void Start()
+    {
+        if (!_playerMover)
+            _playerMover.GetComponent<Mover>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+            MoveToCursor();
+    }
+
+    private void MoveToCursor()
+    {
+        _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(_lastRay, out hit) && _playerMover)
+            _playerMover.MoveTo(hit.point);
+    }
+}
