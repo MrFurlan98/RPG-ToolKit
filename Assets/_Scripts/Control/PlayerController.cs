0bc82409c8ecf446135433deffa03253f3fe8731
Vinicius Furlan <mr.furlan98@gmail.com>
2025-02-12 18:40:14 -0300

    feat: add enemy detection

--- Assets/_Scripts/Control/PlayerController.cs
+++ Assets/_Scripts/Control/PlayerController.cs
@@ -1,21 +1,43 @@
 using UnityEngine;
 using RPGToolKit.Movement;
+using RPGToolKit.Combat;
 
 namespace RPGToolKit.Control
 {
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private Mover _playerMover;
-
-        private Ray _lastRay;
+        [SerializeField] private Fighter _fighter;
 
         private void Start()
         {
             if (!_playerMover)
                 _playerMover.GetComponent<Mover>();
+            if (!_fighter)
+                _fighter.GetComponent<Fighter>();
         }
 
         void FixedUpdate()
+        {
+            UpdateCombat();
+            UpdateMovement();
+        }
+
+        private void UpdateCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (!target) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                    _fighter.Attack(target);
+            }
+        }
+
+        private void UpdateMovement()
         {
             if (Input.GetMouseButton(0))
                 MoveToCursor();
@@ -23,11 +45,15 @@ namespace RPGToolKit.Control
 
         private void MoveToCursor()
         {
-            _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(_lastRay, out hit) && _playerMover)
+            if (Physics.Raycast(GetMouseRay(), out hit) && _playerMover)
                 _playerMover.MoveTo(hit.point);
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
